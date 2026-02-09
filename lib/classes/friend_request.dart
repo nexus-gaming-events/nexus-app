3a75e71da107775721da25acf8fdbd250f32ca92
Nicolò Giallongo <nicolo.giallongo@mail.polimi.it>
2026-02-09 01:03:57 +0100

    access to user page from friend request

--- lib/classes/friend_request.dart
+++ lib/classes/friend_request.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:nexus_app/data_manager.dart';
 import '../constants.dart';
 import 'application_object.dart';
+import '../main.dart';
+import 'user.dart';
 
 class FriendRequest extends ApplicationObject {
   int id;
@@ -34,47 +37,58 @@ class VisualizeFriendRequestPreview extends StatelessWidget {
       ),
       child: Padding(
         padding: EdgeInsets.all(AppConstants.paddingLarge(context)),
-        child: Row(
-          children: [
-            friendRequest.imageUrl.isEmpty
-                ? Image.asset(
-                    'assets/icons/Neil.png',
-                    width: iconSize,
-                    height: iconSize,
-                  )
-                : Image.network(
-                    friendRequest.imageUrl,
-                    width: iconSize,
-                    height: iconSize,
+        child: InkWell(
+            onTap: () async {
+                      NexusAppState.instance!.returnScreenPath.add(
+                        'FriendRequests',
+                      );
+                      NexusAppState.instance!.updateState(
+                        'User',
+                        params: [await DataManager.getUserById(friendRequest.id) ?? User(id: 0, username: 'Unknown', email: '', imageUrl: '', bannerGradient: {'type': 'linear', 'colors': ['0xFF0000FF', '0xFFFF00FF'], 'parameter': 0.0})],
+                      );
+            },
+          child: Row(
+            children: [
+              friendRequest.imageUrl.isEmpty
+                  ? Image.asset(
+                      'assets/icons/Neil.png',
+                      width: iconSize,
+                      height: iconSize,
+                    )
+                  : Image.network(
+                      friendRequest.imageUrl,
+                      width: iconSize,
+                      height: iconSize,
+                    ),
+              SizedBox(width: AppConstants.paddingMedium(context)),
+              Expanded(
+                child: Text(
+                  friendRequest.username,
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: AppConstants.fontSizeLargeResponsive(context),
+                    color: AppConstants.textColor,
                   ),
-            SizedBox(width: AppConstants.paddingMedium(context)),
-            Expanded(
-              child: Text(
-                friendRequest.username,
-                style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: AppConstants.fontSizeLargeResponsive(context),
-                  color: AppConstants.textColor,
                 ),
               ),
-            ),
-            SizedBox(width: AppConstants.paddingMedium(context)),
-            Text(
-                friendRequest.date.toLocal().toString().split(' ')[0],
-                style: TextStyle(
-                  fontSize: AppConstants.fontSizeMediumResponsive(context),
-                  color: AppConstants.semitransparentTextColor,
-                )),
-            IconButton(
-              onPressed: () => {/* Accept friend request logic here */},
-              icon: Icon(Icons.check, color: AppConstants.successColor),
-              ),
-            SizedBox(width: AppConstants.paddingSmall(context)),
-            IconButton(
-              onPressed: () => {/* Decline friend request logic here */},
-              icon: Icon(Icons.close, color: AppConstants.errorColor),
-              ),
-          ],
+              SizedBox(width: AppConstants.paddingMedium(context)),
+              Text(
+                  friendRequest.date.toLocal().toString().split(' ')[0],
+                  style: TextStyle(
+                    fontSize: AppConstants.fontSizeMediumResponsive(context),
+                    color: AppConstants.semitransparentTextColor,
+                  )),
+              IconButton(
+                onPressed: () => {/* Accept friend request logic here */},
+                icon: Icon(Icons.check, color: AppConstants.successColor),
+                ),
+              SizedBox(width: AppConstants.paddingSmall(context)),
+              IconButton(
+                onPressed: () => {/* Decline friend request logic here */},
+                icon: Icon(Icons.close, color: AppConstants.errorColor),
+                ),
+            ],
+          ),
         ),
       ),
     );
